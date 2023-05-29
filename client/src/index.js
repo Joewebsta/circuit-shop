362f6547414e331d7e4b7dbba230891d63cd103e
katstasaph <katherinestasaph@gmail.com>
2023-05-29 14:28:33 -0400

    product listing added

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,39 @@
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+const Actions = () => {
+  return React.createElement("div", {className: "actions product-actions"}, 
+    React.createElement("button", {className: "add-to-cart"}, "Add to Cart"),
+    React.createElement("button", {className: "edit"}, "Edit")
+ );	
+}
+
+const Product = ({productName, price, quantityInStock}) => {
+  return React.createElement("li", {className: "product"},
+    React.createElement("div", {className: "product-details"}, 
+	  React.createElement("h3", null, productName),
+	  React.createElement("p", {className: "price"}, price),
+	  React.createElement("p", {className: "quantity"}, `${quantityInStock} left in stock`),
+	  Actions(),
+	  React.createElement("button", {className: "delete-button"}, "Delete")
+  ));
+}
+
+const ProductListing = () => {
+  return React.createElement("div", {className: "product-listing"},
+  	React.createElement("h2", null, "Products"),
+	React.createElement("ul", {className: "product-list"}, 
+	 React.createElement(Product, {productName: "Amazon Kindle E-reader", price: "$79.99", quantityInStock: 5}),
+	 React.createElement(Product, {productName: "Apple 10.5-Inch iPad Pro", price: "$649.99", quantityInStock: 2}),
+	 React.createElement(Product, {productName: "Yamaha Portable Keyboard", price: "$155.99", quantityInStock: 0}),
+  ));
+}
+
+const Cart = () => {
+  return React.createElement("main", null, 
+    ProductListing(),
+  );
+}
+
 const Header = () => {
   return React.createElement("header", null,
     React.createElement("h1", null, "The Shop!"),
@@ -21,7 +55,7 @@ const Header = () => {
 
 
 const App = () => {
-  return React.createElement("div", { id: "app" }, Header());
+  return React.createElement("div", { id: "app" }, Header(), Cart());
 }
 
 root.render(App());
