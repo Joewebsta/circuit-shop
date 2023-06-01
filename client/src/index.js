93e64c032cc6b0e8a733b2b66c4568afff7db442
katstasaph <katherinestasaph@gmail.com>
2023-06-01 11:58:41 -0400

    implement add new product form

--- client/src/index.js
+++ client/src/index.js
@@ -17,11 +17,11 @@ const Header = () => {
   );
 }
 
-const Products = ({ products, onDisplayNewProductForm, isAddFormVisible }) => {
+const Products = ({ products, onDisplayNewProductForm, onHideNewProductForm, isAddFormVisible, onAddNewProduct }) => {
   return (
     <main>
       <ProductListing products={products} />
-      <AddForm onDisplayNewProductForm={onDisplayNewProductForm} isAddFormVisible={isAddFormVisible} />
+      <AddForm onDisplayNewProductForm={onDisplayNewProductForm} isAddFormVisible={isAddFormVisible} onHideNewProductForm={onHideNewProductForm} onAddNewProduct={onAddNewProduct} />
     </main>
   )
 }
@@ -67,18 +67,48 @@ const Actions = () => {
   )
 }
 
-const AddForm = ({ onDisplayNewProductForm, isAddFormVisible }) => {
+const AddForm = ({ onDisplayNewProductForm, onHideNewProductForm, isAddFormVisible, onAddNewProduct }) => {
+  const [productName, setProductName] = useState("");
+  const [productPrice, setProductPrice] = useState("");
+  const [productQuantity, setProductQuantity] = useState("");
+
+  const handleProductNameChange = (event) => {
+    setProductName(event.target.value);
+  }
+  
+  const handleProductPriceChange = (event) => {
+    setProductPrice(event.target.value);
+  }
+
+  const handleProductQuantityChange = (event) => {
+    setProductQuantity(event.target.value);
+  }
+  
+  const submitForm = (event) => {
+    event.preventDefault();
+	onAddNewProduct(productName, productPrice, productQuantity);
+	clearProductForm();
+  } 
+  
+  const clearProductForm = (event) => {
+    setProductName('');
+	setProductPrice('');
+	setProductQuantity('');
+  }
+  
   return (
     <div className={`add-form ${isAddFormVisible && "visible"}`}>
       <p><button className="add-product-button" onClick={onDisplayNewProductForm}>Add A Product</button></p>
       <h3>Add Product</h3>
-      <form>
+      <form onSubmit = {submitForm} >
         <div className="input-group">
           <label htmlFor="product-name">Product Name:</label>
           <input
             type="text"
             id="product-name"
             name="product-name"
+			value={productName}
+			onChange={handleProductNameChange}
             required
           />
         </div>
@@ -90,6 +120,8 @@ const AddForm = ({ onDisplayNewProductForm, isAddFormVisible }) => {
             name="product-price"
             min="0"
             step="0.01"
+			value={productPrice}
+			onChange={handleProductPriceChange}			
             required
           />
         </div>
@@ -100,12 +132,14 @@ const AddForm = ({ onDisplayNewProductForm, isAddFormVisible }) => {
             id="product-quantity"
             name="product-quantity"
             min="0"
+			value={productQuantity}
+			onChange={handleProductQuantityChange}
             required
           />
         </div>
         <div className="actions form-actions">
           <button type="submit">Add</button>
-          <button type="button">Cancel</button>
+          <button type="button" onClick={onHideNewProductForm} >Cancel</button>
         </div>
       </form>
     </div>
@@ -126,8 +160,21 @@ const App = () => {
     getAllProducts();
   }, []);
 
+  const handleAddNewProduct = async (name, price, quantity) => {
+	const obj = {title: name, price: parseInt(price, 10), quantity: parseInt(quantity, 10) };
+    const response = await axios.post('/api/products', obj);
+	const newProduct = await response.data;
+	const updatedProducts = products.concat(newProduct);
+	setProducts(updatedProducts);
+  }
+
   const handleDisplayNewProductForm = (e) => {
-    setIsAddFormVisible(!isAddFormVisible);
+    setIsAddFormVisible(true);
+  }
+
+  const handleHideNewProductForm = (e) => {
+	console.log("hiding");
+    setIsAddFormVisible(false);
   }
 
   return (
@@ -137,6 +184,8 @@ const App = () => {
         products={products}
         onDisplayNewProductForm={handleDisplayNewProductForm}
         isAddFormVisible={isAddFormVisible}
+		onHideNewProductForm = {handleHideNewProductForm}
+		onAddNewProduct = {handleAddNewProduct}
       />
     </div>
   );
