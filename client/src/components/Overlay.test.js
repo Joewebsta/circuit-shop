89ee9e83146292ea168b371fa79b991da4922f2e
Joe Webster <joseph.scott.webster@gmail.com>
2023-09-08 17:31:17 -0400

    Write overlay tests

---
+++ client/src/components/Overlay.test.js
@@ -0,0 +1,26 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from "react";
+import "@testing-library/jest-dom";
+import { logRoles, render, screen } from "@testing-library/react";
+import Overlay from "./Overlay";
+
+describe("Overlay", () => {
+  test("renders correctly", () => {
+    const { container } = render(<Overlay></Overlay>);
+    expect(container).toBeInTheDocument();
+  });
+
+  test("displays its children correctly", () => {
+    render(
+      <Overlay>
+        <h1>Hello world</h1>
+      </Overlay>
+    );
+
+    const h1Element = screen.getByRole("heading", { level: 1 });
+    expect(h1Element).toBeInTheDocument();
+  });
+});
---
+++ client/src/components/Products/Products.test.js
@@ -0,0 +1,9 @@
+// import React from "react";
+// import { render, screen } from "@testing-library/react";
+// import Products from "./Products";
+
+describe("Products", () => {
+  test("renders correctly", () => {
+    expect(1).toBe(1);
+  });
+});
