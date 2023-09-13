0c58b57ff4626b1354a059504c7f7d5f8978345b
Joe Webster <joseph.scott.webster@gmail.com>
2023-09-13 18:50:32 -0400

    Write CartItem component tests

--- client/src/components/Cart/CartItem.test.js
+++ client/src/components/Cart/CartItem.test.js
@@ -7,37 +7,35 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import CartItem from "./CartItem";
 
-test("test", () => {
-  expect(1).toBe(1);
+describe("CartItem", () => {
+  const item = {
+    createdAt: "2023-09-05T21:14:01.806Z",
+    price: 1299.99,
+    productId: "64f609940669a9a57b74dfb4",
+    quantity: 3,
+    title: "DJI Air 2S",
+    updatedAt: "2023-09-10T18:02:13.661Z",
+    __v: 0,
+    _id: "64f79a19f010a878047df2ca",
+  };
+
+  test("renders cart correctly when cart is empty", () => {
+    render(
+      <table>
+        <tbody>
+          <CartItem item={item} />
+        </tbody>
+      </table>
+    );
+    // screen.debug();
+
+    const productName = screen.getByRole("cell", { name: /DJI Air 2S/i });
+    expect(productName).toBeInTheDocument();
+
+    const productQty = screen.getByRole("cell", { name: "3" });
+    expect(productQty).toBeInTheDocument();
+
+    const productPrice = screen.getByRole("cell", { name: /\$1,299.99/i });
+    expect(productPrice).toBeInTheDocument();
+  });
 });
-
-// const item = {
-//   title: "iPhone",
-//   quantity: 100,
-//   price: 100
-// }
-
-// const renderCartItem = () => {
-//   return render(
-//     <table>
-//       <tbody>
-//         <CartItem item={item} />
-//       </tbody>
-//     </table>
-//   );
-// };
-
-// test('Contains item title', () => {
-//   renderCartItem();
-//   expect(screen.getByText('iPhone')).toBeInTheDocument()
-// });
-
-// test('Contains item quantity', () => {
-//   renderCartItem();
-//   expect(screen.getByText('100')).toBeInTheDocument()
-// });
-
-// test('Contains price quantity', () => {
-//   renderCartItem();
-//   expect(screen.getByText('$100')).toBeInTheDocument()
-// });
--- client/src/components/Products/Products.test.js
+++
@@ -1,9 +0,0 @@
-// import React from "react";
-// import { render, screen } from "@testing-library/react";
-// import Products from "./Products";
-
-describe("Products", () => {
-  test("renders correctly", () => {
-    expect(1).toBe(1);
-  });
-});
